e7935fbd3487f75d94cbc4433eaa901659f83112
Eric Ebulu <ekumamait@Eric-2.local>
2019-08-19 22:23:34 +0200

    chore(dotenv): secure db
    - add dotenv module
    - configure server to use dotenv module
    
    [Delivers #167064107]

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const { PubSub, withFilter, GraphQLServer } = require("graphql-yoga");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
 const typeDefs = require("./typeDefs");
 const resolvers = require('./resolvers');
 
-mongoose.connect("mongodb://localhost/miniChat", {
+dotenv.config();
+
+const { DB_URL } = process.env;
+
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
 const pubsub = new PubSub();
 const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+
 mongoose.connection.once("open", () =>
-  server.start(() => console.log("We make magic over at localhost:4000"))
+  server.start(() => console.log("warming up at localhost:4000"))
 );
